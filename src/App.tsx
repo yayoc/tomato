d261f3dfcf1edae92b86559771d5d09be108330f
yayoc <n.yayoshi+github@gmail.com>
2019-03-06 05:58:32 -0500

    Switching working and breaking timers

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,45 @@ import "./App.css";
 import { CountDown } from "./CountDown";
 
 class App extends Component {
-  onComplete = () => {
-    console.log("completed");
+  state = {
+    isWorking: true
+  };
+
+  onWorkComplete = () => {
+    this.setState({ isWorking: false });
+  };
+
+  onBreakComplete = () => {
+    this.setState({ isWorking: true });
   };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <CountDown
-            initialCount={1000}
-            delay={100}
-            isRunning={true}
-            onComplete={this.onComplete}
-          />
+          {this.state.isWorking ? (
+            <>
+              <p>working</p>
+              <CountDown
+                key="workingCountDown"
+                initialCount={1000}
+                delay={100}
+                isRunning={false}
+                onComplete={this.onWorkComplete}
+              />
+            </>
+          ) : (
+            <>
+              <p>breaking</p>
+              <CountDown
+                key="breakingCountDown"
+                initialCount={500}
+                delay={100}
+                isRunning={true}
+                onComplete={this.onBreakComplete}
+              />
+            </>
+          )}
         </header>
       </div>
     );
