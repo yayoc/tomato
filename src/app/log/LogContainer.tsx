699f1a76a5ea5115d08516da1a25f446ef5298cf
yayoc <n.yayoshi+github@gmail.com>
2019-03-09 17:31:24 -0500

    Display logs on log tab

--- src/app/log/LogContainer.tsx
+++ src/app/log/LogContainer.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useStore } from "../Context";
+import { Work } from "../modules/entity";
 
 export function LogContainer() {
+  const [{ log, entity }] = useStore() as any;
+  const logs = log.workIds.map((id: string) => entity.work[id]);
   return (
     <>
       <h1>Work Log</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>start at</th>
+            <th>end at</th>
+            <th>note</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log: Work) => (
+            <tr key={log.id}>
+              <td>{log.startAt}</td>
+              <td>{log.endAt}</td>
+              <td>{log.note}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
