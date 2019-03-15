9706ed285202a2789317d15ab8e6eabc97aa15c9
yayoc <n.yayoshi+github@gmail.com>
2019-03-15 21:41:06 +0900

    Fix App test

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { StoreProvider } from "./Context";
+import { store } from "./store";
+import { App } from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
